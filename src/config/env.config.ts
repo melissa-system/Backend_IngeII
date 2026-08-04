12a8759b9a6f821d24fcecfcdd7f6386b19d508f
OAiza <power777pro@gmai.com>
2026-08-03 21:51:36 -0600

    cambios en el backend

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,9 @@
 export const EnvConfig = () => ({
-  environment: process.env.NODE_ENV,
-  port: process.env.PORT, // Server port
-  host: process.env.DB_HOST,
-  dbPort: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
 });
